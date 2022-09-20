24b1a07f6273e5ea3a719fb125e07eff3c221817
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-20 14:14:20 -0700

    commit

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export const Register = () => {
   return (
-    <div className='bg-blue-200 h-full flex items-center justify-center'>
-     <div className='flex gap-[15px] rounded-md px-'>
-        <span>Let's chat</span>
-        <span className=''></span>
+    <div className='bg-blue-200 h-[100vh] flex flex-col items-center justify-center'>
+     <div className='flex gap-[10px] flex-col rounded-md py-[20px] px-[60px] justify-center items-center'>
+        <span className='text-purple-600 font-bold text-[24px]'>Let's chat</span>
+        <span className='text-[15px] text-purple-600'>Register</span>
      </div>
-     <form>
-        <input type="text" className="" />
-        <input type="email" className="" />
-        <input type="password" className="" />
-        <input type="file" className="" />
-        <button type="submit">Sign up</button>
+     <form className='flex flex-col gap-[15px]'>
+        <input type="text" placeholder='Display Name' className="p-[15px] border-none w-[250px] placeholder:text-gray-400 border-b-blue-200" />
+        <input type="email" placeholder='Email' className="p-[15px] border-none w-[250px] placeholder:text-gray-400 border-b-blue-200" />
+        <input type="password" placeholder='Password' className="p-[15px] border-none w-[250px] placeholder:text-gray-400 border-b-blue-200" />
+        <input type="file" style={{display:"none"}}  id='avatar' />
+        <label for="avatar" className='cursor-pointer'>
+            <img src="" alt="" />
+             <small>Add an Avatar</small>
+        </label>
+        <button className='bg-blue-500 p-[10px] font-bold cursor-pointer text-white' type="submit">Sign up</button>
      </form>
-     <p>You do have an account? login</p>
+     <p className='text-gray-400 text-[15px] mt-[10px]'>You do have an account? login</p>
     </div>
   )
 }
