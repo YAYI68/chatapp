a73257f2875cd86192f4d048e3de5c0d95409d1f
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-21 05:28:08 -0700

    commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -17,7 +17,7 @@ export const Chats = () => {
            <p className='text-[14px] text-gray-200'>helo</p>
         </div>
       </div> 
-      <div className='hover:bg-gray-900 flex items-center text-white p-[10px] cursor-pointer'>
+      <div className='hover:bg-gray-700 flex items-center text-white p-[10px] cursor-pointer'>
         <img  src='./images/avatar.png' alt='' className='w-[50px] h-[50px]' />
         <div>
            <span className='text-[18px] font-semibold'>Yayi</span>
