08807f4213ccaa0aa584b97c1eb95f977780f74c
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-22 03:18:24 -0700

    commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { onSnapshot, doc, } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useStateContext } from '../contexts/AuthContextProvider';
+import { db } from '../firebaseConfig';
+
 
 export const Chats = () => {
+  
+  const [ chats, setChats] = useState();
+  const { currentUser } = useStateContext();
+  useEffect(() => {
+    
+    const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+     setChats(doc.data())
+  });
+  
+    return () => {
+      unsub()
+    }
+  }, [currentUser.uid])
+  
+console.log(chats)
   return (
     <div>
       <div className='hover:bg-gray-900 flex items-center text-white p-[10px] cursor-pointer'>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs,getDoc, query, setDoc, where, doc, updateDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebaseConfig';
+import { useStateContext } from '../contexts/AuthContextProvider';
 
 
 
@@ -9,7 +10,46 @@ export const Search = () => {
    const [user,setUser] = useState(null)
    const [ error, setError ] = useState(false)
 
+
+   const { currentUser } =  useStateContext();
   
+    console.log({uid:currentUser.uid})
+  
+   const handleSelect = async () =>{
+     const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid
+     
+     try {
+       const res = await getDoc(doc(db, "chats",combinedId))
+        if(!res.exists()){
+          await setDoc(doc(db,"chats",combinedId),{
+            messages:[]
+          });
+          await updateDoc(doc(db,"userChats",currentUser.uid),{
+            [combinedId+".userInfo"]:{
+              uid:user.uid,
+              displayName:user.displayName,
+              photoURL:user.photoURL,
+            },
+            [combinedId+".date"]: serverTimestamp()
+          })
+          await updateDoc(doc(db, "userChats",user.uid),{
+            [combinedId+".userInfo"]:{
+              uid:currentUser.uid, 
+              displayName:currentUser.displayName,
+              photoURL:currentUser.photoURL,
+            },
+            [combinedId+".date"]: serverTimestamp()
+          }) 
+
+       
+        }
+      } catch (error) {
+      
+     }
+     setUser(null);
+     setUsername("");
+    }
+
    const handleSearch = async()=>{
     console.log(username)
     const q = query(collection(db, "users"), where("displayName", "==", username));
@@ -31,16 +71,19 @@ export const Search = () => {
   return (
     <div className='border-b-2 border-b-gray-200'>
       <div className='p-[10px]'>
-        <input onKeyDown={handleKey}  onChange={(e)=>setUsername(e.target.value)} type="search" placeholder='Find a user' className='placeholder:text-gray-200 outline-none bg-transparent  text-white' />
+        <input
+        value={username}
+        onKeyDown={handleKey}  onChange={(e)=>setUsername(e.target.value)} type="search" placeholder='Find a user' className='placeholder:text-gray-200 outline-none bg-transparent  text-white' />
       </div>
       { user &&    
-      <div className='hover:bg-gray-900 flex items-center text-white p-[10px] cursor-pointer'>
-        <img  src={user.photoURL} alt='' className='w-[50px] h-[50px]' />
+      <div  onClick={handleSelect} className='hover:bg-gray-900 flex items-center text-white p-[10px] cursor-pointer'>
+        <img  src={user.photoURL} alt='' className='w-[45px] h-[45px] rounded-full' />
         <div>
-           <span className='text-[18px] font-semibold'>{user.diplayName}</span>
+           <span className='text-[18px] text-white font-semibold ml-4'>{user.displayName}</span>
         </div>
       </div> 
       }
+      { error && <small>User not found</small>}
     </div>
   )
 }
