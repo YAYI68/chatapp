ffaea358458a602668a713b05633a5095fd69952
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-22 05:36:12 -0700

    commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,12 +5,14 @@ import { useStateContext } from '../contexts/AuthContextProvider';
 export const Input = () => {
    const[text,setText] =  useState("")
   const { currentUser,data} = useStateContext();
+  const [ img, setImg ] = useState(null);
+  const [ file, setFile ] = useState();
   return (
     <div className='h-[50px] bg-white p-[10px] flex items-center justify-between'>
-      <input type="text" placeholder="Type Something..." className=' grow-[9] border-none outline-none text-[18px]  text-gray-800 placeholder:text-gray-200' />
+      <input type="text" onChange={(e)=>setText(e.target.value)} placeholder="Type Something..." className=' grow-[9] border-none outline-none text-[18px]  text-gray-800 placeholder:text-gray-200' />
        <div className='flex items-center gap-[15px] justify-between grow-[1] '>
           <img className='h-[24px] cursor-pointer' src="./images/folder.png" alt="" />
-          <input type="file" style={{display:"none"}} name="" id="file" value=""   />
+          <input type="file" onChange={(e)=>setFile(e.target.files[0])} style={{display:"none"}} name="" id="file" value=""   />
           <label for="file">
             <img className='h-[24px] cursor-pointer' src="./images/imglogo.png" alt="" />
           </label>
